44db9055e60f1642c2e5f825b27cb451fce85d8d
Cameron Dutro <camertron@gmail.com>
2013-12-14 12:16:01 -0800

    Initial commit

---
+++ CSKitTranslator/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CSKitTranslator::Application.config.secret_key_base = '1c94704323c5f923ea7cc5432d0f4dcdd674d0f7b6f15d8d88c99f17bdf1a368b64489aacfbbd310d80180cb6b6b9274be22707f4177002e2ad1f8655b523b42'
