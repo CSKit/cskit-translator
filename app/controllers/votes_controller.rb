5efd16fb9221ccbd465efa381715ef667bfb25fa
Cameron Dutro <cdutro@twitter.com>
2014-02-25 19:46:52 -0800

    Up/down voting uniquified per user/phrase

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,24 +1,28 @@
 class VotesController < ApplicationController
+
+  UPVOTE_VALUE = 1
+  DOWNVOTE_VALUE = -1
+
   before_filter :authenticate_user!
+  before_filter :set_passage
+  before_filter :set_translation
 
   def upvote
-    @passage = Phrase.find(params[:phrase_id])
-    @translation = Translation.find(params[:translation_id])
-
-    if Vote.create(user: current_user, translation: @translation, value: 1)
-      flash[:notice] = "Vote saved."
-      redirect_to phrase_path(@passage)
-    else 
-      flash[:alert] = "Error - vote not saved."
-      redirect_to phrase_path(@passage)
-    end
+    render_vote(
+      perform_vote(UPVOTE_VALUE)
+    )
   end
 
   def downvote
-    @passage = Phrase.find(params[:phrase_id])
-    @translation = Translation.find(params[:translation_id])
+    render_vote(
+      perform_vote(DOWNVOTE_VALUE)
+    )
+  end
 
-    if Vote.create(user: current_user, translation: @translation, value: -1)
+  private
+
+  def render_vote(vote)
+    if vote.save
       flash[:notice] = "Vote saved."
       redirect_to phrase_path(@passage)
     else 
@@ -27,5 +31,22 @@ class VotesController < ApplicationController
     end
   end
 
+  def perform_vote(options = {})
+    vote_for(current_user, @translation).tap do |vote|
+      vote.assign_attributes(value: options.fetch(:vote_value, UPVOTE_VALUE))
+    end
+  end
+
+  def vote_for(user, translation)
+    Vote.where(user: user, translation: translation).first_or_initialize
+  end
+
+  def set_passage
+    @passage = Phrase.find(params[:phrase_id])
+  end
+
+  def set_translation
+    @translation = Translation.find(params[:translation_id])
+  end
 
 end
